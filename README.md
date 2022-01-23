4d8467711b074e3cb11c2fde3960ec2eae36b688
guessfinger <373898092@qq.com>
2022-01-23 19:16:18 +0800

    stylelint文件配置

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # stylelint_common_config
+
+- 之前配置一直不生效，从以下参考文档中获取的，然后就可以配置成功了
+    1. https://juejin.cn/post/7026998283155275807  
+    2. https://gitee.com/ww7758/vue3-tempalte/tree/master
+
+
+
+- 官方参考规范
+  http://stylelint.docschina.org/user-guide/rules/
---
+++ demo/test.vue
@@ -0,0 +1,26 @@
+<template>
+  {{ a }}
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  data: function () {
+    return {
+      a: 132,
+    }
+  },
+})
+</script>
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
